9ca2d8514dd7ec0f23b51aebf34b8e2bbaa09c53
afgana23 <126515757+afgana23@users.noreply.github.com>
2023-06-12 15:49:54 -0700

    Add files via upload

---
+++ calculation.js
@@ -0,0 +1,63 @@
+
+         $(document).ready(function(){
+           $('#one').click(function(){
+             document.forms.result.value += 1;
+           });
+           $('#two').click(function(){
+             document.forms.result.value += 2;
+           });
+           $('#three').click(function(){
+             document.forms.result.value += 3;
+           });
+           $('#four').click(function(){
+             document.forms.result.value += 4;
+           });
+           $('#five').click(function(){
+             document.forms.result.value += 5;
+           });
+           $('#six').click(function(){
+             document.forms.result.value += 6;
+           });
+           $('#seven').click(function(){
+             document.forms.result.value += 7;
+           });
+           $('#eight').click(function(){
+             document.forms.result.value += 8;
+           });
+           $('#nine').click(function(){
+             document.forms.result.value += 9;
+           });
+           $('#zero').click(function(){
+             document.forms.result.value += 0;
+           });
+           $('#add').click(function(){
+             document.forms.result.value += '+';
+           });
+           $('#subs').click(function(){
+             document.forms.result.value += '-';
+           });
+           $('#multi').click(function(){
+             document.forms.result.value += '*';
+           });
+           $('#divide').click(function(){
+             document.forms.result.value += '/';
+           });
+           $('#dot').click(function(){
+             document.forms.result.value += '.';
+           });
+           $('#equal').click(function(){
+             if (result.value == "") {
+               alert("Please enter any numbers to calculate!");
+             }else{
+               document.forms.result.value =
+               eval(document.forms.result.value);
+             }
+           });
+           $('#clear').click(function(){
+             document.forms.result.value = "";
+           });
+           $('#del').click(function(){
+            document.forms.result.value = "";
+          });
+         });
+      
